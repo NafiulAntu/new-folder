a6a2fa38eb7bb61aaec1c0d0adc968c1077b1ef3
NafiulAntu <nafiul.nia@gmail.com>
2026-01-10 21:24:54 +0600

    add more

---
+++ complete-javascript-course-master/complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,96 @@
+  /*
+  let js = 'ammazing';
+  console.log(40 + 8 + 23 -10);
+  console.log('Antu');
+  console.log(23);
+  let firstName = "Nafiul";
+  console.log(firstName);
+  let Antu_nafiul = "NIA";
+  let $function = 27;
+  let PI = 3.1415;
+  let myFirstJob = 'Programmer';
+  let myCurrentJob = 'Teacher';
+  console.log(myFirstJob);
+  */
+
+  /* 
+  true;
+  console.log(true);
+
+  let javascriptIsFun = true;
+
+  console.log(javascriptIsFun);
+  console.log(typeof true);
+  
+  console.log.apply(typeof javascriptIsFun);
+  console.log(typeof 23);
+  console.log(typeof 'Nafiul');
+
+  javascriptIsFun = 'Yes!';
+  console.log(typeof javascriptIsFun);
+
+  year = 1991;
+  console.log(year);
+  console.log(typeof null);
+*/
+
+/*
+   let age = 30;
+   age = 31;
+   
+   const birthYear = 1991;
+   //birthYear = 1990;
+ 
+   var job = 'programmer';
+   job = 'teacher';
+*/
+
+
+  //  const ageJons = 2037 - 1991 ;
+  //  console.log(ageJons);
+
+  //math oparators
+  //  const now = 2037 ;
+  //  const ageJons1 = now - 1991;
+  //  const ageSarah = now - 2018;
+  //  console.log(ageJons1,ageSarah);
+
+  //  console.log(ageJons1 * 2 , ageJons1/10,2**3);
+
+   // 2**3 means 2 to the power of 3 = 2*2*2 
+
+  //  const firstName = 'Nafiul';
+  //  const lastName = 'Islam';
+  //  console.log(firstName + ' ' + lastName);
+
+   //Assingment oparators
+  //  let x = 10 + 5;
+  //  x+=10;
+  //  x*=4;
+  //  x++;
+  //  x--;
+  //  console.log(x);
+
+   //comparison oparators
+
+  //  console.log(ageJons1>ageSarah);
+  //  console.log(ageSarah>=18);
+
+  //  const isFullAge = ageSarah >=18;
+  //  console.log(isFullAge);
+
+/* Write your code below. Good luck! 🙂 */
+
+const massMark = 78 ;
+const heightMark =1.69 ;
+const massJohn =95 ;
+const heightJohn =1.95 ;
+
+const BMIMark = massMark / (heightMark**2);
+const BMIJohn = massJohn / (heightJohn**2);
+
+const markHigherBMI = BMIMark > BMIJohn;
+
+console.log(BMIMark,BMIJohn,markHigherBMI);
+
+  
