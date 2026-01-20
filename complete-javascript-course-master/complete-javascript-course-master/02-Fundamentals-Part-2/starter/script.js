a006a624fdf2b32b4a0732ebb6e5348c44d1286b
NafiulAntu <nafiul.nia@gmail.com>
2026-01-20 22:43:42 +0600

    add

--- complete-javascript-course-master/complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
+++ complete-javascript-course-master/complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
@@ -1,12 +1,34 @@
-'use strict';
+// 'use strict';
 
 
-let hasDriversLicense = false;
-const passTest = true;
+// let hasDriversLicense = false;
+// const passTest = true;
 
-if (passTest) hasDriversLicense = true ;
-if (hasDriversLicense) console.log('I can drive :D');
+// if (passTest) hasDriversLicense = true ;
+// if (hasDriversLicense) console.log('I can drive :D');
 
 //const interface = 'Audio';
 
-const privete = 534;
+//const privete = 534;
+
+function logger(){
+    console.log('My name is Jonas');
+}
+
+// calling / running / invoking function
+logger();
+logger();
+logger();
+
+function fruitProcessor(apples, oranges){
+    console.log(apples, oranges);
+    const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+    return juice;
+}
+
+const appleJuice = fruitProcessor(5,0);
+console.log(appleJuice);
+console.log(fruitProcessor(5,0));
+
+const appleOrangeJuice = fruitProcessor(2,4);
+console.log(appleOrangeJuice);
