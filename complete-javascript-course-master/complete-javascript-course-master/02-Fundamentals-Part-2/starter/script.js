f4632386ce90e2a459609aced9c7e9cb325311b3
NafiulAntu <nafiul.nia@gmail.com>
2026-01-21 01:41:30 +0600

    add

--- complete-javascript-course-master/complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
+++ complete-javascript-course-master/complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
@@ -11,24 +11,44 @@
 
 //const privete = 534;
 
-function logger(){
-    console.log('My name is Jonas');
-}
+// function logger(){
+//     console.log('My name is Jonas');
+// }
 
 // calling / running / invoking function
-logger();
-logger();
-logger();
+// logger();
+// logger();
+// logger();
 
-function fruitProcessor(apples, oranges){
-    console.log(apples, oranges);
-    const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
-    return juice;
+// function fruitProcessor(apples, oranges){
+//     console.log(apples, oranges);
+//     const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+//     return juice;
+// }
+
+// const appleJuice = fruitProcessor(5,0);
+// console.log(appleJuice);
+// console.log(fruitProcessor(5,0));
+
+// const appleOrangeJuice = fruitProcessor(2,4);
+// console.log(appleOrangeJuice);
+
+// const num = Number('23');
+
+
+// Function Declaration
+function calcAge1 (birthYeah) {
+    return 2037 - birthYeah;
 }
 
-const appleJuice = fruitProcessor(5,0);
-console.log(appleJuice);
-console.log(fruitProcessor(5,0));
+const age1 = calcAge1(1991);
 
-const appleOrangeJuice = fruitProcessor(2,4);
-console.log(appleOrangeJuice);
+// Function Expression
+const calcAge2 = function (birthYeah) {
+    return 2037 - birthYeah;
+}
+
+const age2 = calcAge2(1991);
+
+console.log(age1,age2);
+
