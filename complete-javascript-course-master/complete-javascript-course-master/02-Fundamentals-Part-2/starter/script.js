25277569599b33448967af94cc8f6fb8abdcc345
NafiulAntu <nafiul.nia@gmail.com>
2026-01-22 18:02:11 +0600

    add

--- complete-javascript-course-master/complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
+++ complete-javascript-course-master/complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
@@ -57,20 +57,35 @@
 
 
 
-// Arrow Function
-const calcAge3 = birthYeah => 2037 - birthYeah;
-const age3 = calcAge3(1991);
+// // Arrow Function
+// const calcAge3 = birthYeah => 2037 - birthYeah;
+// const age3 = calcAge3(1991);
 
-//calcAge3(1991);
-console.log(age3);
+// //calcAge3(1991);
+// console.log(age3);
 
-const yearsUntilRetirement = (birthYeah, firstName) => {
-    const age = 2037 - birthYeah;
-    const retirement = 65 - age ;
-   // return retirement;
-    return `${firstName} retires in ${retirement} years`;
+// const yearsUntilRetirement = (birthYeah, firstName) => {
+//     const age = 2037 - birthYeah;
+//     const retirement = 65 - age ;
+//    // return retirement;
+//     return `${firstName} retires in ${retirement} years`;
+// }
+
+// console.log(yearsUntilRetirement(1991, 'Jonas')); 
+// console.log(yearsUntilRetirement(1980, 'Bob'));
+
+
+
+function cutFruitPieces(fruit){
+    return fruit * 3;
 }
 
-console.log(yearsUntilRetirement(1991, 'Jonas')); 
-console.log(yearsUntilRetirement(1980, 'Bob'));
+function fruitProcessor(apples, oranges){
+    const applePieces = cutFruitPieces(apples);
+    const orangePieces = cutFruitPieces(oranges);
+    const juice = `Juice with ${applePieces} pieces of apple and ${orangePieces} pieces of orange.`;
+    return juice;
+}
 
+const juice = fruitProcessor(2,3);
+console.log(juice);
