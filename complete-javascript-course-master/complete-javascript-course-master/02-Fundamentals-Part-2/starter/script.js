c4a4699c93018927664db3da95b37df14ddddfcb
NafiulAntu <nafiul.nia@gmail.com>
2026-01-22 21:11:23 +0600

    add

--- complete-javascript-course-master/complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
+++ complete-javascript-course-master/complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
@@ -113,37 +113,46 @@
 // console.log(yearsUntilRetirement(1950, 'Bob'));
 
 
-const friend1 = 'Michael';
-const friend2 = 'Steven';
-const friend3 = 'Peter';
+// const friend1 = 'Michael';
+// const friend2 = 'Steven';
+// const friend3 = 'Peter';
+
+// const friends = ['Michael', 'Steven', 'Peter'];
+// console.log(friends);
+
+// const years = new Array(1991, 1984, 2008, 2020);
+// console.log(friends[0]);
+// console.log(friends[2]);
+
+// console.log(friends.length);
+// console.log(friends[friends.length - 1]);
+
+// friends[2] = 'Jay';
+// console.log(friends); 
+
+// //friends = ['Bob','Alice'];
+// const firstName = 'Jonas';
+// const jonas = [firstName, 'Schmedtmann', 2037 - 1991, 'teacher', friends];
+
+// console.log(jonas);
+
+// // Exercise
+// const calcAge = function(birthYeah){
+//     return 2037 - birthYeah;
+// }
+// const years1 = [1990, 1967, 2002, 2010, 2018];
+
+
+// const age1 = calcAge(years1[0]);
+// const age2 = calcAge(years1[1]);
+// const age3 = calcAge(years1[years1.length - 1]);
+// console.log(age1, age2, age3);
+
+
+// const ages = [calcAge(years1[0]), calcAge(years1[1]), calcAge(years1[years1.length - 1])];
+// console.log(ages);
+
 
 const friends = ['Michael', 'Steven', 'Peter'];
+friends.push('Jay');
 console.log(friends);
-
-const years = new Array(1991, 1984, 2008, 2020);
-console.log(friends[0]);
-console.log(friends[2]);
-
-console.log(friends.length);
-console.log(friends[friends.length - 1]);
-
-friends[2] = 'Jay';
-console.log(friends); 
-
-//friends = ['Bob','Alice'];
-const firstName = 'Jonas';
-const jonas = [firstName, 'Schmedtmann', 2037 - 1991, 'teacher', friends];
-
-console.log(jonas);
-
-// Exercise
-const calcAge = function(birthYeah){
-    return 2037 - birthYeah;
-}
-const years1 = [1990, 1967, 2002, 2010, 2018];
-
-
-const age1 = calcAge(years1[0]);
-const age2 = calcAge(years1[1]);
-const age3 = calcAge(years1[years1.length - 1]);
-console.log(age1, age2, age3);
