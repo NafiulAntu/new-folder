b197fac4cc0034b59bbc631339af82e90a49701a
NafiulAntu <nafiul.nia@gmail.com>
2026-01-27 18:04:03 +0600

    add

--- complete-javascript-course-master/complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
+++ complete-javascript-course-master/complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
@@ -315,22 +315,32 @@
 // }
 
 
-const jonas = [
-    'Jonas',
-    'Schmedtmann',
-    2037 - 1991,
-    'teacher',
-    ['Michael', 'Peter', 'Steven'],
-    true
-];
+// const jonas = [
+//     'Jonas',
+//     'Schmedtmann',
+//     2037 - 1991,
+//     'teacher',
+//     ['Michael', 'Peter', 'Steven'],
+//     true
+// ];
 
-for (let i = jonas.length -1 ; i >=0; i--){
-    console.log(i, jonas[i]);
+// for (let i = jonas.length -1 ; i >=0; i--){
+//     console.log(i, jonas[i]);
+// }
+
+// for (let exercise = 1; exercise < 4; exercise++){
+//     console.log(`---- Starting exercise ${exercise} ----`); 
+//     for (let rep = 1; rep < 6; rep++){
+//         console.log(`Exercise ${exercise} : Lifting weight repetition ${rep} 🏋️‍♀️`);
+//     }
+// }
+
+for (let rep = 1; rep <= 10; rep++){
+    console.log(`Lifting weights repetition ${rep} 🏋️‍♀️`);
 }
 
-for (let exercise = 1; exercise < 4; exercise++){
-    console.log(`---- Starting exercise ${exercise} ----`); 
-    for (let rep = 1; rep < 6; rep++){
-        console.log(`Exercise ${exercise} : Lifting weight repetition ${rep} 🏋️‍♀️`);
-    }
+let rep = 1;
+while (rep <= 10){
+    console.log(`Lifting weights repetition ${rep} 🏋️‍♀️`);
+    rep++;
 }
