a94dc0fcb4dc39e3e0fa642cf1fd0c0faeef3d68
NafiulAntu <nafiul.nia@gmail.com>
2026-02-22 14:33:56 +0600

    add

--- complete-javascript-course-master/complete-javascript-course-master/03-Developer-Skills/starter/script.js
+++ complete-javascript-course-master/complete-javascript-course-master/03-Developer-Skills/starter/script.js
@@ -7,6 +7,13 @@
 // console.log(calcAge(1981));
 // console.log(calcAge(2000));
 
+// PROBLEM 1:
+// We work for a company building a smart home thermometer.
+// Our most recent task is this:
+// "Given an array of temperatures of one day,
+// calculate the temperature amplitude.
+// Keep in mind that sometimes there might be a sensor error."
+
 const temperatures = [3, -2, -6, -1, 'error', 9, 13, 17, 15, 14, 9, 5];
 
 // 1) Understanding the problem
@@ -35,3 +42,30 @@ const calcTempAplitude = function (temps) {
 
 const amplitude = calcTempAplitude(temperatures);
 console.log(amplitude);
+
+// PROBLEM 2:
+// Function should now receive 2 arrays of temps
+
+// 1) Understanding the problem
+// - With 2 arrays, should we implement functionality twice? NO! Just merge two arrays
+
+// 2) Breaking up into sub-problems
+// - Merge 2 arrays
+
+const calcTempAplitudeNew = function (t1, t2) {
+  const temps = t1.concat(t2);
+  console.log(temps);
+  let max = temps[0];
+  let min = temps[0];
+  for (let i = 0; i < temps.length; i++) {
+    const curTemp = temps[i];
+
+    if (max < curTemp) max = curTemp;
+    if (min > curTemp) min = curTemp;
+  }
+  console.log(max, min);
+  return max - min;
+};
+
+const amplitudeNew = calcTempAplitudeNew([3, 5, 1], [9, 0, 5]);
+console.log(amplitudeNew);
