ecb2a473b852bef313131168fc5891b2d21c8a9b
NafiulAntu <nafiul.nia@gmail.com>
2026-03-02 22:43:32 +0600

    add

--- complete-javascript-course-master/complete-javascript-course-master/05-Guess-My-Number/starter/script.js
+++ complete-javascript-course-master/complete-javascript-course-master/05-Guess-My-Number/starter/script.js
@@ -2,7 +2,7 @@
 
 console.log(document.querySelector('.message').textContent);
 
-// document.querySelector('.message').textContent = 'Correct Number!';
+document.querySelector('.message').textContent = 'Correct Number!';
 // document.querySelector('.number').textContent = 13;
 // document.querySelector('.score').textContent = 10;
 // document.querySelector('.guess').value = 23;
