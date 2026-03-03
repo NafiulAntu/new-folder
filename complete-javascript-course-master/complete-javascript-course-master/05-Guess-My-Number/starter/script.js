2e873b0dc5d352dbf24d1e4197efff0727cae3ef
NafiulAntu <nafiul.nia@gmail.com>
2026-03-03 22:54:06 +0600

    add

--- complete-javascript-course-master/complete-javascript-course-master/05-Guess-My-Number/starter/script.js
+++ complete-javascript-course-master/complete-javascript-course-master/05-Guess-My-Number/starter/script.js
@@ -13,7 +13,8 @@ document.querySelector('.guess').value = 23;
 console.log(document.querySelector('.guess').value);
 */
 
-const number = Math.random();
+const number = Math.trunc(Math.random() * 20) + 1;
+document.querySelector('.number').textContent = number;
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
