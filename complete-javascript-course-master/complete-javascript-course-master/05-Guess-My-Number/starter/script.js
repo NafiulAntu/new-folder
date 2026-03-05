20a22cf58d77a166965e9ca855c6bbc14d77bfde
NafiulAntu <nafiul.nia@gmail.com>
2026-03-05 22:45:51 +0600

    add

--- complete-javascript-course-master/complete-javascript-course-master/05-Guess-My-Number/starter/script.js
+++ complete-javascript-course-master/complete-javascript-course-master/05-Guess-My-Number/starter/script.js
@@ -14,6 +14,7 @@ console.log(document.querySelector('.guess').value);
 */
 
 const number = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
 document.querySelector('.number').textContent = number;
 
 document.querySelector('.check').addEventListener('click', function () {
@@ -25,8 +26,22 @@ document.querySelector('.check').addEventListener('click', function () {
   } else if (guess === number) {
     document.querySelector('.message').textContent = 'Correct Number!';
   } else if (guess > number) {
-    document.querySelector('.message').textContent = 'Too high!';
+    if (score > 1) {
+      document.querySelector('.message').textContent = 'Too high!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = 'You lost the game!';
+      document.querySelector('.score').textContent = 0;
+    }
   } else if (guess < number) {
-    document.querySelector('.message').textContent = 'Too low!';
+    if (score > 1) {
+      document.querySelector('.message').textContent = 'Too low!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = 'You lost the game!';
+      document.querySelector('.score').textContent = 0;
+    }
   }
 });
