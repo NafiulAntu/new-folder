341d437e8b3cb5bc312f3e2a5ff322f741dd3783
NafiulAntu <nafiul.nia@gmail.com>
2026-03-06 21:34:30 +0600

    add

--- complete-javascript-course-master/complete-javascript-course-master/05-Guess-My-Number/starter/script.js
+++ complete-javascript-course-master/complete-javascript-course-master/05-Guess-My-Number/starter/script.js
@@ -13,9 +13,9 @@ document.querySelector('.guess').value = 23;
 console.log(document.querySelector('.guess').value);
 */
 
-const number = Math.trunc(Math.random() * 20) + 1;
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
-document.querySelector('.number').textContent = number;
+document.querySelector('.number').textContent = secretNumber;
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
@@ -27,13 +27,13 @@ document.querySelector('.check').addEventListener('click', function () {
   }
 
   // When player wins
-  else if (guess === number) {
+  else if (guess === secretNumber) {
     document.querySelector('.message').textContent = 'Correct Number!';
     document.querySelector('body').style.backgroundColor = '#60b347';
     document.querySelector('.number').style.width = '30rem';
   }
   // When guess is too high
-  else if (guess > number) {
+  else if (guess > secretNumber) {
     if (score > 1) {
       document.querySelector('.message').textContent = 'Too high!';
       score--;
@@ -44,7 +44,7 @@ document.querySelector('.check').addEventListener('click', function () {
     }
 
     // When guess is too low
-  } else if (guess < number) {
+  } else if (guess < secretNumber) {
     if (score > 1) {
       document.querySelector('.message').textContent = 'Too low!';
       score--;
@@ -55,3 +55,11 @@ document.querySelector('.check').addEventListener('click', function () {
     }
   }
 });
+
+(document.querySelector('.again').addEventListener('click'),
+  function () {
+    score = 20;
+    secretNumber = Math.trunc(Math.random() * 20) + 1;
+    document.querySelector('.message').textContent = 'Start gussing...';
+    document.querySelector('.score').textContent = score;
+  });
