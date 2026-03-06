3f796138f5adb6b06feb7b02d6c046fbf8352223
NafiulAntu <nafiul.nia@gmail.com>
2026-03-06 21:49:15 +0600

    add

--- complete-javascript-course-master/complete-javascript-course-master/05-Guess-My-Number/starter/script.js
+++ complete-javascript-course-master/complete-javascript-course-master/05-Guess-My-Number/starter/script.js
@@ -35,7 +35,7 @@ document.querySelector('.check').addEventListener('click', function () {
 
     document.querySelector('body').style.backgroundColor = '#60b347';
     document.querySelector('.number').style.width = '30rem';
-
+    // Update highscore
     if (score > highscore) {
       highscore = score;
       document.querySelector('.highscore').textContent = highscore;
