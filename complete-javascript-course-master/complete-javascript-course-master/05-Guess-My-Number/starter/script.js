e3bb78775867347d77657ffebda3f34d9fa94ae4
NafiulAntu <nafiul.nia@gmail.com>
2026-03-09 21:41:26 +0600

    add

--- complete-javascript-course-master/complete-javascript-course-master/05-Guess-My-Number/starter/script.js
+++ complete-javascript-course-master/complete-javascript-course-master/05-Guess-My-Number/starter/script.js
@@ -66,3 +66,5 @@ document.querySelector('.again').addEventListener('click', function () {
   document.querySelector('body').style.backgroundColor = '#222';
   document.querySelector('.number').style.width = '15rem';
 });
+
+//porar time da change koro
