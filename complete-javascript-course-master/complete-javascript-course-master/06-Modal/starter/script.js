95bed98c14fed23fa2b782849464a52c918eb9c5
NafiulAntu <nafiul.nia@gmail.com>
2026-03-13 22:40:03 +0600

    add

--- complete-javascript-course-master/complete-javascript-course-master/06-Modal/starter/script.js
+++ complete-javascript-course-master/complete-javascript-course-master/06-Modal/starter/script.js
@@ -15,6 +15,12 @@ for (let i = 0; i < btnsOpenModal.length; i++) {
   });
 }
 
+const openModal = function () {
+  console.log('Button clicked');
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
 const closeModal = function () {
   modal.classList.add('hidden');
   overlay.classList.add('hidden');
